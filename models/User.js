f716e66b6b73a69dd566d5d9dcf581c6cec647b9
Robel <rofikre@yahoo.com>
2022-03-31 22:49:07 -0400

    created the User model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        friends: [],
+    }
+);
+
+const User = model('User', UserSchema);
+
+module.exports = User;
