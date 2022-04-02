363bcaee61c852c43ebf84930ee31890722c7378
Robel <rofikre@yahoo.com>
2022-04-02 03:22:27 -0400

    cerated the thougth api routes and tested GET,POST,PUT,DELETE routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+const {
+    getThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+// /api/thoughts/
+router
+    .route('/')
+    .get(getThoughts)
+    .post(addThought)
+
+// /api/thoughts/<thoughtId>
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought)
+    
+// /api/<thoughtId>reactions/<reactionId>
+router
+  .route('/:thoughtId/reactions/reactionId')
+  .post(addReaction)
+  .delete(deleteReaction)
+
+  
+
+module.exports = router;
